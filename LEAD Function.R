f761d74d1f7eeb161d3f3988ff098d90c949b58e
Andrew Perelson <aperelson@gmail.com>
2017-09-15 11:40:19 +0800

    V2.

--- LEAD Function.R
+++ LEAD Function.R
@@ -1,19 +1,18 @@
-loadLEADSurvey <- function() { 
-  library(readxl)
-
+loadLEADSurvey <- function() {
   dirToUse <- 'C:\\Users\\Andrew\\Dropbox\\Development\\LEAD 1'
   setwd(dirToUse)
-  folder.names <- list.dirs(full.names = TRUE)
-
+  folder.names <- list.dirs(full.names = TRUE, recursive = FALSE)
+  
   result <- sapply(folder.names[-1], function(x) {
     file.names <- list.files(x, full.names = TRUE, pattern =".csv")  
-
+    
     if (length(file.names))
     {
       for (j in 1:length(file.names)) {
         fileName <- file.names[j]
         
         file.contents <- read.csv(file=fileName, header=TRUE, sep=",")
+        file.contents <- file.contents[-c(1),]
       }
     }
   })
