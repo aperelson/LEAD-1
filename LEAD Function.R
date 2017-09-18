983f0931875a56e7da372a06224da6f2d4abe429
aperelson <aperelson@gmail.com>
2017-09-18 20:57:17 +0800

    next

--- LEAD Function.R
+++ LEAD Function.R
@@ -1,7 +1,19 @@
+# function to merge columns into one with a space separator and then
+# remove multiple spaces
+returnValueFromVector <- function(df, cols) {
+  exp <- paste('df[,', cols, ']', sep='', collapse=',' )
+  nexp <- paste(" paste(", exp, ", sep=' ')")
+  newcol <- eval(parse(text=nexp))
+  newcol <- gsub('  *', ' ', newcol)
+  newcol <- gsub('^ *', '', newcol)
+  gsub(' *$', '', newcol)
+}
+
+
 loadLEADSurvey <- function() {
   ##dirToUse <- 'C:\\Users\\Andrew\\Dropbox\\Development\\LEAD 1'
   dirToUse <- 'C:\\Users\\perel_000\\Documents\\GitHub\\LEAD-1'
-  
+
   setwd(dirToUse)
   folder.names <- list.dirs(full.names = TRUE, recursive = FALSE)
   
@@ -18,6 +30,7 @@ loadLEADSurvey <- function() {
         
         ##Determine some basic info:
         weekNumber <- strftime(as.Date(substr(unique(file.contents[1,3]),1,10),"%m/%d/%Y"),format="%W") 
+        
         if (grepl('The week to come', fileName))
         {
           surveyType <- 0
@@ -26,6 +39,11 @@ loadLEADSurvey <- function() {
         {
           surveyType <- 1
         }
+        
+        teamSelection <- returnValueFromVector(file.contents, c(10:21))
+        teamPosition <- returnValueFromVector(file.contents, c(22:30))
+        tookTimeToReflect <- returnValueFromVector(file.contents, c(31:35))
+        tookTimeToReflectComments <- file.contents[,c(36:36)]
       }
     }
   })
